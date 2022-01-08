5c84a124c1fc31a8cb359cf9026d6b96dce71d55
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-01-08 14:41:01 +0000

    Removing vommented out code

--- pythonCode/Latifi_Twitter/keys.py
+++
@@ -1,7 +0,0 @@
-keyDict = {
-    "onsumer_key" : "pbtHr2f5Jem0Mw6tuBxqlM5ma",
-    "consumer_secret" : "FSsbRYZ8HO91IYSAGR1mhrVCup3exo7MmfYG7KUb3nORsy8bKB",
-    "access_token" : "1223559208538464261-Vvvvoh80NkgjIhMaqHiv4wRZtTaadr",
-    "access_token_secret" : "sapm6YirVZQ3pVcRpIPI4wRnDoI0F9Is1Rhh1ionGNZzY",
-    "bearer_token" : "AAAAAAAAAAAAAAAAAAAAABzVXQEAAAAAHm87kjhADCWD8qL%2FIUDqjkV13VE%3DosUJ8QPWvuZl2ldDxdl1cK9VC9V4KSdqEA8oQQmSNJpqKGmZti"
-}
