9f0670b8f50dab7c23b92eec49bdfd6e38b252a3
ofithcheallaigh <sofithcheallaigh@gmail.com>
2022-01-15 19:55:11 +0000

    updating md file structure and adding md file

--- pythonCode/Markdown/markdown_example_1/work_markdown.py
+++ pythonCode/Markdown/markdown_example_1/work_markdown.py
@@ -1,16 +1,27 @@
 from mdutils.mdutils import MdUtils
 from mdutils import Html
 from datetime import datetime
+import calendar
 
+week_day = datetime.today().strftime('%A')
 today = datetime.today().strftime('%Y-%m-%d')
 title = 'Work ' + today
-# print(title)
+
+
 
 # file_name is the name we will see in a folder
 # title is the title of the doc, inside the file
-mdFile = MdUtils(file_name=title, title='Work Notes')
+mdFile = MdUtils(file_name=title, title='Work Notes '+week_day+' '+today)
+mdFile.new_paragraph("Current project: BLE")
+
+mdFile.new_header(level=1,title="Boot Time")
+mdFile.new_paragraph("Boot up time today was: ")
+
+# Add in a to do list
+mdFile.new_header(level=1,title="To Do List")
+mdFile.new_paragraph("[ ]")
 
 
-
+mdFile.new_table_of_contents(table_title='Contents', depth=2)
 # Needed to actually generate the markdown file
 mdFile.create_md_file()
